7f8e9b940c01155f9730aed162d39762ddfc1cee
Matthew Johnson <mattjj@csail.mit.edu>
2015-11-08 14:41:59 -0500

    move plotting code into a function

--- negbin_maxlike.py
+++ negbin_maxlike.py
@@ -3,8 +3,8 @@ import autograd.numpy as np
 import autograd.numpy.random as npr
 from autograd.scipy.special import gammaln
 from autograd import grad, multigrad
-
 import scipy.optimize
+import matplotlib.pyplot as plt
 
 
 '''
@@ -39,6 +39,17 @@ def fit_maxlike(x, r_guess):
     return r, p(r)
 
 
+def plot_results(data, r, p):
+    xm = data.max()
+    plt.figure()
+    plt.hist(data, bins=np.arange(xm+1)-0.5, normed=True, label='normed data counts')
+    plt.xlim(0,xm)
+    plt.plot(np.arange(xm), np.exp(negbin_loglike(r, p, np.arange(xm))), label='maxlike fit')
+    plt.xlabel('k')
+    plt.ylabel('p(k)')
+    plt.legend(loc='best')
+
+
 if __name__ == "__main__":
     # generate data
     npr.seed(0)
@@ -56,13 +67,5 @@ if __name__ == "__main__":
     grad_both = multigrad(loglike, argnums=[0,1])
     print(grad_both(r, p))
 
-    import matplotlib.pyplot as plt
-    xm = data.max()
-    plt.figure()
-    plt.hist(data, bins=np.arange(xm+1)-0.5, normed=True, label='normed data counts')
-    plt.xlim(0,xm)
-    plt.plot(np.arange(xm), np.exp(negbin_loglike(r, p, np.arange(xm))), label='maxlike fit')
-    plt.xlabel('k')
-    plt.ylabel('p(k)')
-    plt.legend(loc='best')
+    plot_results(data, r, p)
     plt.show()
