5f49e5b9ac48bd99027975ab12825d8e473802da
Matthew Johnson <mattjj@csail.mit.edu>
2015-11-09 10:15:29 -0500

    remove weird non-printing characters from load.py

--- spikestim/load.py
+++ spikestim/load.py
@@ -4,7 +4,7 @@ from scipy.io import loadmat
 
 def load():
     '''
-    Data has a field called ‘stimulus’  that contains 15 fields, one for each distinct stimulus.
+    Data has a field called 'stimulus' that contains 15 fields, one for each distinct stimulus.
     For each stimulus there are 167 fields, one for each neuron.
     For each neuron there are 3 variables:
         - spikeCountsAll trials, a vector of 10 spike counts, one response for each trial;
