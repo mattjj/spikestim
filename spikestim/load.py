5375c2825d3a98f4f5351b4113cf3629c52d3afb
Matthew Johnson <mattjj@csail.mit.edu>
2015-11-09 10:26:19 -0500

    add load_all_spikecounts convenience function

--- spikestim/load.py
+++ spikestim/load.py
@@ -2,6 +2,11 @@ from __future__ import division
 from scipy.io import loadmat
 
 
+getfield = lambda name: lambda lst: map(lambda obj: getattr(obj, name), lst)
+neurons = getfield('neuron')
+allspikes = getfield('spikeCountsAllTrials')
+
+
 def load():
     '''
     Data has a field called 'stimulus' that contains 15 fields, one for each distinct stimulus.
@@ -11,4 +16,19 @@ def load():
         - spikeCountsMean;
         - spikeCountsVar.
     '''
-    return loadmat('data/data.mat', squeeze_me=True, struct_as_record=False)
+    return loadmat('data/data.mat', squeeze_me=True, struct_as_record=False)['data']
+
+
+def load_all_spikecounts():
+    '''
+    Returns a list of 15 stimuli, each element is a list of 167 neurons, each a
+    list of 10 trails.
+
+        data = load_all_spikecounts()
+        for stim_idx, neurons in enumerate(data):
+            for neuron_idx, trials in enumerate(neurons):
+                print 'Avg. response from neuron {} to stimulus {}: {}'\
+                    .format(neuron_idx, stim_idx, np.mean(trials))
+    '''
+    raw = load()
+    return map(allspikes, neurons(raw.stimulus))
